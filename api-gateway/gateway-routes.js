5f4207657641e9bcfd51abe84d31e6141fb7da88
Ahmet Kasap <aahmetkasap@gmail.com>
2024-01-03 02:59:50 +0300

    gateway routes

---
+++ api-gateway/gateway-routes.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const router = express.Router()
+
+const proxy = require('express-http-proxy')
+const { checkToken } = require('./src/middlewares/auth')
+
+
+
+//!------------------ FAVORİTES SERVİCE --------------------------
+
+//* ---- get favorites, add favorites, delete favorites ----
+
+router.use('/api/v1/users/favorites', checkToken, proxy('http://localhost:5001/', {
+    proxyReqPathResolver: (req) => {
+      return req.originalUrl;
+    },
+
+    proxyReqOptDecorator: (proxyReqOpts, originalReq) => {
+      //console.log(originalReq, proxyReqOpts)
+
+      proxyReqOpts.headers['userId'] = originalReq.user._id
+      return proxyReqOpts
+
+    }
+}))
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = router
+
